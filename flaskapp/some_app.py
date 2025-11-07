3d70a11414d75d0ba405b0c9adb1d738f2ace008
cvistyn-gh <cvistyn87@gmail.com>
2025-11-07 13:42:46 +0500

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,15 +1,11 @@
 print("Hello world")
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><head></head><body>Hello Worls!</body></html>"
-
-if __name__ == "__main__":
-    app.run(host='127.0.0.1',port=5000)
-
-from flask import render_template
+    return "<html><head></head><body>Hello World!</body></html>"
 
 @app.route("/data_to")
 def data_to():
@@ -17,3 +13,6 @@ def data_to():
     some_str = 'Hello my dear friends!'
     some_value = 10
     return render_template('simple.html',some_str=some_str,some_value=some_value,some_pars=some_pars)
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1',port=5000)
