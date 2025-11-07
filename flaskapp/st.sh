f0f85ad1aa8f74bf22c19dd74c297ea1936f7578
cvistyn-gh <cvistyn87@gmail.com>
2025-11-07 13:55:15 +0500

    Update st.sh

--- flaskapp/st.sh
+++ flaskapp/st.sh
@@ -2,6 +2,13 @@ gunicorn --bind 127.0.0.1:5000 wsgi:app & APP_PID=$!
 sleep 5
 echo start client
 python3 client.py
+CLIENT_EXIT_CODE=$?
 echo $APP_PID
 kill -TERM $APP_PID
-exit 0 
+if [ $CLIENT_EXIT_CODE -ne 0 ]; then
+    echo "Тест завершился с ошибкой: $CLIENT_EXIT_CODE"
+    exit $CLIENT_EXIT_CODE
+else
+    echo "Тест завершился успешно"
+    exit 0
+fi
